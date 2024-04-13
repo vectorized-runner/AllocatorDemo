9a79ef8fbed87b9ea276a7bd0f555d8a9ac0e1e4
Bartu Baş <bartubas99@gmail.com>
2024-04-13 10:01:35 +0300

    LinearAllocator setup

---
+++ Assets/Code/LinearAllocator.cs
@@ -0,0 +1,23 @@
+using System;
+using Unity.Collections;
+
+namespace AllocatorDemo
+{
+	public struct LinearAllocator
+	{
+		private AllocatorHelper<LinearAllocatorImpl> _helper;
+
+		// For ease-of-use
+		public static implicit operator Allocator(LinearAllocator val) => val._helper.Allocator.ToAllocator;
+
+		// For ease-of-use
+		public static implicit operator AllocatorManager.AllocatorHandle(LinearAllocator val) =>
+			val._helper.Allocator.Handle;
+
+		public LinearAllocator(Span<byte> memory)
+		{
+			// Temp backing Allocator as LinearAllocator lives in stack anyway
+			_helper = new AllocatorHelper<LinearAllocatorImpl>(Allocator.Temp);
+		}
+	}
+}
