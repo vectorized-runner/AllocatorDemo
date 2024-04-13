e377a8e8d45799e16523cfb1d5e9bf2760255a05
Bartu Baş <bartubas99@gmail.com>
2024-04-13 09:41:45 +0300

    Add IsCreated for Dispose tracking

--- Assets/Code/CustomAllocatorStruct.cs
+++ Assets/Code/CustomAllocatorStruct.cs
@@ -8,18 +8,32 @@ namespace AllocatorDemo
 
 		public ref CustomAllocator RefAllocator => ref Helper.Allocator;
 
+		public bool IsCreated;
+
 		// For ease-of-use
 		public static implicit operator Allocator(CustomAllocatorStruct str) => str.RefAllocator.ToAllocator;
+
+		// For ease-of-use
+		public static implicit operator AllocatorManager.AllocatorHandle(CustomAllocatorStruct str) =>
+			str.RefAllocator.Handle;
 
 		public CustomAllocatorStruct(int dummy)
 		{
 			Helper = new AllocatorHelper<CustomAllocator>(Allocator.Persistent);
+			IsCreated = true;
 		}
 
 		public void Dispose()
 		{
+			if (!IsCreated)
+			{
+				// throw new InvalidOperationException($"Attempting to Dispose already Destroyed or Uninitialized {nameof(CustomAllocatorStruct)}");
+				return;
+			}
+
 			RefAllocator.Dispose();
 			Helper.Dispose();
+			IsCreated = false;
 		}
 	}
 }
